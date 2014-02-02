16b6ba39940c50cd46d77a7aed8d7699104c7fd8
SevenBits <meokid2@yahoo.com>
2014-02-02 13:06:39 -0500

    Updated site CSS.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+/* Site wide Javascript. This stuff is run on every page. */
 
+function setupLayout() {
+    setInterval('adjustLayout();', 1000);
+}
+
+setupLayout();
+
+function adjustLayout() {
+    try {
+        var divB = $get('.main-page-content');
+        var divAHeight = 20px;
+        divB.style.height = document.body.clientHeight - divAHeight ;
+    }
+    catch (e) { }
+}
